000a944bc881dfe22f6c9bfdb75ce75ca439ac9e
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2022-03-27 15:29:28 -0600

    Add files via upload

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Hi</h1>
+    <b-spinner label="Loading..."></b-spinner>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage'
+})
+</script>
+
